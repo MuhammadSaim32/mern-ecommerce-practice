b2c24a28b173270767e967047a4b37f83116c4f2
MuhammadSaim32 <Muhammad.saim.saleem.3.8@gmail.com>
2025-10-23 15:12:53 +0500

    added .env

--- Front-End/src/backend/axios.api.js
+++ Front-End/src/backend/axios.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:3000/api/v1",
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/v1`,
   timeout: 50000,
 });
 
@@ -13,4 +13,3 @@ api.interceptors.response.use(
 );
 
 export default api;
-

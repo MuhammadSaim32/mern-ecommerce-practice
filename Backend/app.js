5f1f383a4d4bbfc4311aadf9c3f060708f59976f
MuhammadSaim32 <Muhammad.saim.saleem.3.8@gmail.com>
2025-10-23 15:59:23 +0500

    testing

--- Backend/app.js
+++ Backend/app.js
@@ -1,23 +1,28 @@
-import express from 'express'
-import UserRouter from './src/routes/route.users.js'
-import productRouter from './src/routes/route.product.js'
-import paymentRouter from './src/routes/route.payment.js'
-import cors from "cors"
-import cookieParser from 'cookie-parser'
-const app = express()
+import express from "express";
+import UserRouter from "./src/routes/route.users.js";
+import productRouter from "./src/routes/route.product.js";
+import paymentRouter from "./src/routes/route.payment.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173", // Allow frontend URL
     credentials: true, // Allow cookies
-}))
+  }),
+);
 
+app.get("/hello", (req, res) => {
+  res.send("worker find");
+});
 
-app.use('/api/v1/payment',paymentRouter)
+app.use("/api/v1/payment", paymentRouter);
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/v1/users',UserRouter)
-app.use('/api/v1/products',productRouter)
+app.use("/api/v1/users", UserRouter);
+app.use("/api/v1/products", productRouter);
 
+export default app;
 
-export default app

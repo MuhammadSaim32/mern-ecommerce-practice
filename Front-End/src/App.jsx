35cdc16dbc164cef12e11a96c0084b2716704203
MuhammadSaim32 <muhammad.saim.saleem.3.8@gmail.com>
2025-10-24 11:22:00 +0500

    testing

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import Home from './components/Home'
-import {Outlet} from 'react-router-dom'
-import {NavBar,Footer} from "./components/export"
+import { useState } from "react";
+import Home from "./components/Home";
+import { Outlet } from "react-router-dom";
+import { NavBar, Footer } from "./components/export";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-     <>
-     <NavBar/>
-  <Outlet/>
-  <Footer/>
-     </>
-
-  )
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
